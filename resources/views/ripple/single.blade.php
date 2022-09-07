ce269ef06004b6290aa8a7dc73c7d0b787dde50c
phantom0803 <sonlv.wru@gmail.com>
2022-09-07 16:11:50 +0000

    add modal trailer - single.blade

--- resources/views/ripple/single.blade.php
+++ resources/views/ripple/single.blade.php
@@ -41,24 +41,56 @@
                 <img class="w-full cursor-pointer rounded-t-lg" style="aspect-ratio: 256/340" src="{{ $movie->thumb_url }}"
                     alt="" />
                 <div class="flex py-3 justify-between">
-                    <div class="flex w-full justify-center space-x-2">
-                        @if ($movie->status == 'trailer' && $movie->trailer_url)
-                            <a class="bg-[#d9534f] hover:bg-opacity-80 text-gray-50 inline-block px-2 py-1 rounded"
-                                title="{{ $movie->name }} - {{ $movie->origin_name }} ({{ $movie->publish_year }})"
-                                href="{{ $movie->trailer_url }}" target="__blank">Xem trailer
-                            </a>
-                        @elseif($movie->status != 'trailer' && count($movie->episodes) && $movie->episodes[0]['link'] != '')
+                    <div class="w-full text-center space-x-2">
+                        @if ($movie->trailer_url)
+                            <label for="trailer-modal"
+                                class="modal-button bg-sky-500 hover:bg-opacity-80 text-gray-50 inline-block px-2 py-1 rounded cursor-pointer">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                    stroke-width="1.5" stroke="currentColor" class="w-4 h-4 inline">
+                                    <path stroke-linecap="round"
+                                        d="M15.75 10.5l4.72-4.72a.75.75 0 011.28.53v11.38a.75.75 0 01-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25h-9A2.25 2.25 0 002.25 7.5v9a2.25 2.25 0 002.25 2.25z" />
+                                </svg>
+                                Trailer
+                            </label>
+                        @endif
+                        @if ($movie->status != 'trailer' && count($movie->episodes) && $movie->episodes[0]['link'] != '')
                             <a class="bg-red-600 hover:bg-opacity-80 text-gray-50 inline-block px-2 py-1 rounded"
                                 title="{{ $movie->name }} - {{ $movie->origin_name }} ({{ $movie->publish_year }})"
-                                href="{{ $movie->episodes->sortByDesc('name', SORT_NATURAL)->first()->getUrl() }}">Xem phim
+                                href="{{ $movie->episodes->sortByDesc('name', SORT_NATURAL)->first()->getUrl() }}">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                    stroke-width="1.5" stroke="currentColor" class="w-4 h-4 inline">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="M3.375 19.5h17.25m-17.25 0a1.125 1.125 0 01-1.125-1.125M3.375 19.5h1.5C5.496 19.5 6 18.996 6 18.375m-3.75 0V5.625m0 12.75v-1.5c0-.621.504-1.125 1.125-1.125m18.375 2.625V5.625m0 12.75c0 .621-.504 1.125-1.125 1.125m1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125m0 3.75h-1.5A1.125 1.125 0 0118 18.375M20.625 4.5H3.375m17.25 0c.621 0 1.125.504 1.125 1.125M20.625 4.5h-1.5C18.504 4.5 18 5.004 18 5.625m3.75 0v1.5c0 .621-.504 1.125-1.125 1.125M3.375 4.5c-.621 0-1.125.504-1.125 1.125M3.375 4.5h1.5C5.496 4.5 6 5.004 6 5.625m-3.75 0v1.5c0 .621.504 1.125 1.125 1.125m0 0h1.5m-1.5 0c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125m1.5-3.75C5.496 8.25 6 7.746 6 7.125v-1.5M4.875 8.25C5.496 8.25 6 8.754 6 9.375v1.5m0-5.25v5.25m0-5.25C6 5.004 6.504 4.5 7.125 4.5h9.75c.621 0 1.125.504 1.125 1.125m1.125 2.625h1.5m-1.5 0A1.125 1.125 0 0118 7.125v-1.5m1.125 2.625c-.621 0-1.125.504-1.125 1.125v1.5m2.625-2.625c.621 0 1.125.504 1.125 1.125v1.5c0 .621-.504 1.125-1.125 1.125M18 5.625v5.25M7.125 12h9.75m-9.75 0A1.125 1.125 0 016 10.875M7.125 12C6.504 12 6 12.504 6 13.125m0-2.25C6 11.496 5.496 12 4.875 12M18 10.875c0 .621-.504 1.125-1.125 1.125M18 10.875c0 .621.504 1.125 1.125 1.125m-2.25 0c.621 0 1.125.504 1.125 1.125m-12 5.25v-5.25m0 5.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125m-12 0v-1.5c0-.621-.504-1.125-1.125-1.125M18 18.375v-5.25m0 5.25v-1.5c0-.621.504-1.125 1.125-1.125M18 13.125v1.5c0 .621.504 1.125 1.125 1.125M18 13.125c0-.621.504-1.125 1.125-1.125M6 13.125v1.5c0 .621-.504 1.125-1.125 1.125M6 13.125C6 12.504 5.496 12 4.875 12m-1.5 0h1.5m-1.5 0c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125M19.125 12h1.5m0 0c.621 0 1.125.504 1.125 1.125v1.5c0 .621-.504 1.125-1.125 1.125m-17.25 0h1.5m14.25 0h1.5" />
+                                </svg>
+                                Xem phim
                             </a>
-                        @else
-                            <p class="text-white">Đang cập nhật...</p>
                         @endif
+
+                        @if (!$movie->trailer_url && count($movie->episodes) && $movie->episodes[0]['link'] == '')
+                         <div class="text-white">Đang cập nhật...</div>
+                        @endif
+
                     </div>
                 </div>
             </div>
         </div>
+
+        @if ($movie->trailer_url)
+            <input type="checkbox" id="trailer-modal" class="modal-toggle" />
+            <div class="modal">
+                <div class="modal-box relative bg-black/60">
+                    <label for="trailer-modal" class="btn btn-sm btn-circle bg-red-600 text-white absolute right-2 top-2">✕</label>
+                    @php
+                        parse_str(parse_url($movie->trailer_url, PHP_URL_QUERY), $parse_url);
+                        $trailer_url = $parse_url['v'];
+                    @endphp
+                    <iframe width="100%" height="315" src="https://www.youtube.com/embed/{{ $trailer_url }}"
+                        title="YouTube video player" frameborder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        frameborder="0" scrolling="no" allowfullscreen></iframe>
+                </div>
+            </div>
+        @endif
 
         <div class="w-full sm:w-1/2 md:grow mt-3 sm:mt-0">
             <div class="w-full rounded-lg p-3 text-[#bbb] bg-[#1511116d]">
